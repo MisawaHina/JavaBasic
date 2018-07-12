287b679c0af647938240c1bfb6d2f518191a4c0b
DESKTOP-91LTQ5K\mysyw <mysywy@gmail.com>
2018-07-12 16:44:50 +0900

    11-3

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -11,10 +11,29 @@ public class PTra11_02 {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
 
+		//FileReaderClass book = new FileReaderClass();
+
+		Book[] books = FileReaderClass.readBookDataFile();
+
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
 
+		String line = scanner.nextLine();
+
+		for(int i = 0; i < books.length; i++) {
+
+			  String str = line;
+
+		        if (str.contains(books[i].title))
+		        {
+		            System.out.println(books[i].title);
+		        }
+
+		}
+
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
+
 
 	}
 }
